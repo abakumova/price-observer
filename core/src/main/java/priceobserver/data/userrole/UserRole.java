1771e2247702b90f8f63368dbd9c85d82856b779
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2020-03-16 01:42:44 +0200

    PO-2 - started development of DB entities.

--- core/src/main/java/priceobserver/data/userrole/UserRole.java
+++ core/src/main/java/priceobserver/data/userrole/UserRole.java
@@ -1,4 +1,36 @@
 package priceobserver.data.userrole;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user_role")
 public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
