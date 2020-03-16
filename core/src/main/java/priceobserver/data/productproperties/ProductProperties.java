19e25269b9a46bdd0856c0985dc72280184ef8e8
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2020-03-16 02:53:51 +0200

    PO-2 - started development of DB entities 1.

--- core/src/main/java/priceobserver/data/productproperties/ProductProperties.java
+++ core/src/main/java/priceobserver/data/productproperties/ProductProperties.java
@@ -1,4 +1,42 @@
 package priceobserver.data.productproperties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "product_properties")
 public class ProductProperties {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String properties;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProperties() {
+        return properties;
+    }
+
+    public void setProperties(String properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductProperties{" +
+                "id=" + id +
+                ", properties='" + properties + '\'' +
+                '}';
+    }
 }
