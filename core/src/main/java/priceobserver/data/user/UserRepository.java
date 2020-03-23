ce94d3c3bd363ae28c19c3a385d1f70d0bee9bfb
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-03-23 22:06:08 +0200

    PO-2 - added MySQL connector dependency and small improvements.

--- core/src/main/java/priceobserver/data/user/UserRepository.java
+++ core/src/main/java/priceobserver/data/user/UserRepository.java
@@ -2,6 +2,10 @@ package priceobserver.data.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    Optional<User> findByEmailAndPassword(String email, String encryptedPassword);
+    Optional<User> findByEmail(String email);
 }
