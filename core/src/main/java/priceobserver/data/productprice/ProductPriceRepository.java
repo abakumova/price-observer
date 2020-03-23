b8fb2832df6a78bd5959a3ba00eab02ae4d62567
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-03-23 03:23:38 +0200

    PO-2 - created ProductPriceRepository, ProductRepository, UserRepository, WishListRepository.

---
+++ core/src/main/java/priceobserver/data/productprice/ProductPriceRepository.java
@@ -0,0 +1,7 @@
+package priceobserver.data.productprice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductPriceRepository extends CrudRepository<ProductPrice, Long> {
+
+}
---
+++ core/src/main/java/priceobserver/data/wishlist/WishListRepository.java
@@ -0,0 +1,7 @@
+package priceobserver.data.wishlist;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WishListRepository extends CrudRepository<WishList, Long> {
+
+}
