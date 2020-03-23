18def736d614ac9abeb33ad1c0d22a9f83c0556e
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-03-23 19:32:18 +0200

    PO-2 - some improvements in StoreRepositoryTest, added find method in StoreRepository, made Maven run Unit-tests and specified a default Maven goal to run by "mvn" command.

--- core/src/main/java/priceobserver/data/store/StoreRepository.java
+++ core/src/main/java/priceobserver/data/store/StoreRepository.java
@@ -2,6 +2,9 @@ package priceobserver.data.store;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface StoreRepository extends CrudRepository<Store, Long> {
 
+    Optional<Store> findFirstByName(String name);
 }
--- core/src/test/java/priceobserver/data/store/StoreRepositoryTest.java
+++ core/src/test/java/priceobserver/data/store/StoreRepositoryTest.java
@@ -1,18 +1,18 @@
 package priceobserver.data.store;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class StoreRepositoryTest {
 
@@ -20,20 +20,27 @@ public class StoreRepositoryTest {
     private static final String STORE_NAME_2 = "Store_2";
     private static final String STORE_NAME_3 = "Store_3";
 
+    private static Store store1;
+    private static Store store2;
+    private static Store store3;
+
     @Autowired
     private StoreRepository storeRepository;
 
-    @Test
-    public void shouldSaveStoreToDb() {
-        Store store1 = new Store();
+    @BeforeAll
+    static void setUp() throws Exception {
+        store1 = new Store();
         store1.setName(STORE_NAME_1);
 
-        Store store2 = new Store();
+        store2 = new Store();
         store2.setName(STORE_NAME_2);
 
-        Store store3 = new Store();
+        store3 = new Store();
         store3.setName(STORE_NAME_3);
+    }
 
+    @Test
+    public void shouldSaveStoresToDb() {
         storeRepository.saveAll(Arrays.asList(store1, store2, store3));
 
         assertEquals(3, storeRepository.count());
@@ -42,5 +49,25 @@ public class StoreRepositoryTest {
         assertTrue(stores.stream().anyMatch(s -> s.getName().equals(STORE_NAME_1)));
         assertTrue(stores.stream().anyMatch(s -> s.getName().equals(STORE_NAME_2)));
         assertTrue(stores.stream().anyMatch(s -> s.getName().equals(STORE_NAME_3)));
+    }
+
+    @Test
+    public void shouldFindStoreByPassedName() {
+        storeRepository.saveAll(Arrays.asList(store1, store2, store3));
+
+
+        assertTrue(storeRepository.findFirstByName(STORE_NAME_1).isPresent());
+        assertTrue(storeRepository.findFirstByName(STORE_NAME_2).isPresent());
+        assertTrue(storeRepository.findFirstByName(STORE_NAME_3).isPresent());
+    }
+
+    @Test
+    public void shouldDeleteStoreFromDb() {
+        storeRepository.saveAll(Arrays.asList(store1, store2, store3));
+        Optional<Store> store = storeRepository.findFirstByName(store1.getName());
+
+        assertTrue(store.isPresent());
+        store.ifPresent(value -> storeRepository.delete(value));
+        assertFalse(storeRepository.findFirstByName(STORE_NAME_1).isPresent());
     }
 }
