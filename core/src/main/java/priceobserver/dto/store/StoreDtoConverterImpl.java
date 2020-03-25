f4ecebc39409fc41728de3929f192aa38cba8ec7
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-03-25 11:09:08 +0200

    PO-3 - decided to use Spring library to copy properties from DTO and Entity and vise versa.

--- core/src/main/java/priceobserver/dto/store/StoreDtoConverterImpl.java
+++ core/src/main/java/priceobserver/dto/store/StoreDtoConverterImpl.java
@@ -1,24 +1,22 @@
 package priceobserver.dto.store;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 import priceobserver.data.store.Store;
-import priceobserver.data.store.StoreBuilder;
 
 @Component
 public class StoreDtoConverterImpl implements StoreDtoConverter {
     @Override
     public StoreDto convertToDto(Store entity) {
-        return StoreDtoBuilder.aStoreDto()
-                .withId(entity.getId())
-                .withName(entity.getName())
-                .build();
+        StoreDto storeDto = new StoreDto();
+        BeanUtils.copyProperties(entity, storeDto);
+        return storeDto;
     }
 
     @Override
     public Store convertToEntity(StoreDto dto) {
-        return StoreBuilder.aStore()
-                .withId(dto.getId())
-                .withName(dto.getName())
-                .build();
+        Store storeEntity = new Store();
+        BeanUtils.copyProperties(dto, storeEntity);
+        return storeEntity;
     }
 }
