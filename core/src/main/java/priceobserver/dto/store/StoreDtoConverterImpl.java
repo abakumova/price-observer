1d5faecbc55f21021c43a227b232537aa7144b4b
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-03-25 11:10:23 +0200

    PO-3 - added static import.

--- core/src/main/java/priceobserver/dto/store/StoreDtoConverterImpl.java
+++ core/src/main/java/priceobserver/dto/store/StoreDtoConverterImpl.java
@@ -1,22 +1,23 @@
 package priceobserver.dto.store;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 import priceobserver.data.store.Store;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 @Component
 public class StoreDtoConverterImpl implements StoreDtoConverter {
     @Override
     public StoreDto convertToDto(Store entity) {
         StoreDto storeDto = new StoreDto();
-        BeanUtils.copyProperties(entity, storeDto);
+        copyProperties(entity, storeDto);
         return storeDto;
     }
 
     @Override
     public Store convertToEntity(StoreDto dto) {
         Store storeEntity = new Store();
-        BeanUtils.copyProperties(dto, storeEntity);
+        copyProperties(dto, storeEntity);
         return storeEntity;
     }
 }
