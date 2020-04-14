c88c68fab1ec0ed2a3eddfdc7bbb1514b13665b5
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-14 17:01:47 +0300

    PO-36 - applied new variant of 404 page

--- webapp/src/main/java/priceobserver/controller/CustomErrorController.java
+++ webapp/src/main/java/priceobserver/controller/CustomErrorController.java
@@ -5,29 +5,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CustomErrorController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomErrorController.class);
+    private static final String PAGE404 = "error/page404";
+    private static final String ERROR = "error";
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Optional<Integer> status = Optional.ofNullable(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE))
+                .map(Object::toString)
+                .map(Integer::parseInt);
 
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                LOG.error("ERROR 404 Page not found");
-                return "page404";
-            }
+        if (status.isPresent() && status.get() == HttpStatus.NOT_FOUND.value()) {
+            LOGGER.error(String.format("ERROR 404 URL %s?%s", request.getRequestURL().toString(), request.getQueryString()));
+            return PAGE404;
         }
-        return "error";
+
+        return ERROR;
     }
 
     @Override
