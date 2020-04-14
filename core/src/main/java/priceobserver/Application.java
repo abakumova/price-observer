02e9912b525e76980fc8f86ba6872c7f8bec0c95
Evgeniy Kiprenko <43073571+evgeniykiprenko@users.noreply.github.com>
2020-04-14 10:36:46 +0300

    PO-39 - added controllers for our pages. (#23)
    * PO-39 - added controllers for our pages. Added Thymeleaf support to pages. Set logging level to DEBUG.
    
    * PO-39 - removed typo.
    
    * PO-39 - moved to css files height and width values for images
    
    * PO-39 - replaced Log in label to Main in login.html.
    
    Co-authored-by: Evgeniy Kiprenko <zhenyakipenko@gmail.com>

--- core/src/main/java/priceobserver/Application.java
+++ core/src/main/java/priceobserver/Application.java
@@ -3,6 +3,12 @@ package priceobserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * To start the application run this class main method.
+ * By default, the application starts on 8080 port.
+ *
+ * Note: do not forget to specify DB credentials at resources/application.properties
+ */
 @SpringBootApplication
 public class Application {
 
