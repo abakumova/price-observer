4f43611936e15ee0e9426189a5981e3849639656
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-04-16 01:23:48 +0300

    PO-48 - workpiece for Parser. Print products names.

--- parser/src/main/java/priceovserver/parser/Parser.java
+++ parser/src/main/java/priceovserver/parser/Parser.java
@@ -2,13 +2,21 @@ package priceovserver.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import priceobserver.dto.product.ProductDto;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * It's just a first parser version. We need to get more experience in this deal before we start.
+ */
 public class Parser {
 
     private static final String AVIC_PAGE_WITH_IPHONES = "https://avic.ua/iphone.html";
+    private static List<ProductDto> products = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -16,6 +24,10 @@ public class Parser {
 
         Elements elements = document.getElementsByClass("box-product");
 
-        elements.forEach((System.out::println));
+        elements.forEach(Parser::populateProductList);
+    }
+
+    private static void populateProductList(Element element) {
+        System.out.println(element.select("span.name > a").first().text());
     }
 }
