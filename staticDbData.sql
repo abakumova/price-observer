6b8210db3fc33877699cc04c254e8606dd9f9c68
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-23 20:45:18 +0300

    PO-58 - added static data into manufacturer table

--- staticDbData.sql
+++ staticDbData.sql
@@ -9,3 +9,22 @@ VALUES (1, "admin", "admin", "admin@gmail.com", '2020-01-01', "admin", 3),
        (3, "Viktoriia", "Abakumova", "abakumovaviktory@gmail.com", '1999-07-08', "admin", 1),
        (4, "Test", "Test", "test@gmail.com", '2020-02-02', "test", 2),
        (5, "User", "User", "user@gmail.com", '2020-03-03', "test", 2);
+
+INSERT INTO `price_observer`.`manufacturer` (`name`, `country`)
+VALUES ("Apple", "USA"),
+       ("Samsung", "South Korea"),
+       ("Xiaomi", "China"),
+       ("Huawei", "China"),
+       ("Meizu", "China"),
+       ("OnePlus", "China"),
+       ("Philips", "Netherlands"),
+       ("Bosch", "Germany"),
+       ("Asus", "Taiwan"),
+       ("Acer", "Taiwan"),
+       ("LG", "South Korea"),
+       ("Lenovo", "Hong Kong"),
+       ("HP", "USA"),
+       ("Dell", "USA"),
+       ("Microsoft", "USA"),
+       ("JBL", "USA"),
+       ("Sony", "Japan");
