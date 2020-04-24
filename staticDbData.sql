82cc36c9f8a39c3c499479ed8a130134ed26f7ce
Viktoriia <34584226+abakumova@users.noreply.github.com>
2020-04-24 14:11:36 +0300

    PO-57 - added static data into product_types (#33)

--- staticDbData.sql
+++ staticDbData.sql
@@ -9,3 +9,11 @@ VALUES (1, "admin", "admin", "admin@gmail.com", '2020-01-01', "admin", 3),
        (3, "Viktoriia", "Abakumova", "abakumovaviktory@gmail.com", '1999-07-08', "admin", 1),
        (4, "Test", "Test", "test@gmail.com", '2020-02-02', "test", 2),
        (5, "User", "User", "user@gmail.com", '2020-03-03', "test", 2);
+
+INSERT INTO `price_observer`.`product_type` (`name`)
+VALUES ("smartphone"),
+       ("laptop"),
+       ("tablet"),
+       ("smartwatch"),
+       ("earphones"),
+       ("TV");
