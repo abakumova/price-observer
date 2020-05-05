7d2389b16c906206a89c9a530b7957aa0a34ff0f
Evgeniy Kiprenko <43073571+evgeniykiprenko@users.noreply.github.com>
2020-05-05 11:21:54 +0300

    PO-48 - Parse first site (#32)

---
+++ parser/src/main/java/priceobserver/ProductParser.java
@@ -0,0 +1,34 @@
+package priceobserver;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import priceobserver.dto.product.ProductDto;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ProductParser {
+
+    /**
+     * Get a list of Products by given URL to the products page.
+     * For example, URL may look like "https://avic.ua/iphone.html".
+     *
+     * @param avicUrlWithProduct URL to products page
+     * @return list of products
+     */
+    List<ProductDto> parse(String avicUrlWithProduct);
+
+    /**
+     * Returns a document by given URL.
+     *
+     * @param url URL to document
+     * @return document by given URL.
+     */
+    default Document getPageByUrl(String url) {
+        try {
+            return Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new ProductParsingException(String.format("Error during getting document by provided url. Url: %s ", url), e);
+        }
+    }
+}
---
+++ parser/src/main/java/priceobserver/avic/AvicProductParser.java
@@ -0,0 +1,226 @@
+package priceobserver.avic;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import priceobserver.ImageSaverImpl;
+import priceobserver.ProductParser;
+import priceobserver.dto.product.ProductDto;
+import priceobserver.dto.product.ProductDtoBuilder;
+import priceobserver.dto.productproperties.ProductPropertiesDto;
+import priceobserver.dto.productproperties.ProductPropertiesDtoBuilder;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * The class to parse products from Avic store site.
+ */
+@Component
+public class AvicProductParser implements ProductParser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AvicProductParser.class);
+
+    private static final String AVIC_DOMAIN = "https://avic.ua/";
+
+    private static final List<Document> productPages = new ArrayList<>();
+    private static final List<ProductDto> products = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ProductParser parser = new AvicProductParser();
+        /*
+          https://avic.ua/macbook.html
+          https://avic.ua/iphone.html
+          https://avic.ua/ipad.html
+          https://avic.ua/imac.html
+          https://avic.ua/apple-watch-umnie-chasi.html
+         */
+        List<ProductDto> productDtos = parser.parse("https://avic.ua/apple-watch-umnie-chasi.html");
+        LOGGER.info("products list size {}", productDtos.size());
+        productDtos.forEach(e -> LOGGER.info("\n{}\n", e));
+    }
+
+    @Override
+    public List<ProductDto> parse(String avicUrlWithProduct) {
+        productPages.clear();
+        products.clear();
+        LOGGER.info("Started parsing of the site {}", avicUrlWithProduct);
+        fillProductPagesList(avicUrlWithProduct);
+        productPages.forEach(this::extractProductAndAddToList);
+        return products;
+    }
+
+    /**
+     * Parse product page gallery to get product pages.
+     */
+    private void fillProductPagesList(String avicUrlWithProduct) {
+        // Open the first part of page with products, get all product pages from it.
+        Document startingPageWithProducts = getPageByUrl(avicUrlWithProduct);
+        Elements linksToProductPages = startingPageWithProducts.select("div.images > a.img");
+        linksToProductPages.forEach(a -> productPages.add(getPageByUrl(a.attr("href"))));
+
+        //    The page with products is split into a few pages.
+        //    Since we parsed the first part of it, we open others and getting product pages.
+        Elements linksToNextPages = startingPageWithProducts.select("div.paging > a.ditto_page");
+        List<Document> otherPagesWithProducts = new ArrayList<>();
+        linksToNextPages.forEach(a -> otherPagesWithProducts.add(getPageByUrl(AVIC_DOMAIN.concat(a.attr("href")))));
+
+        for (Document doc : otherPagesWithProducts) {
+            linksToProductPages = doc.select("div.images > a.img");
+            linksToProductPages.forEach(a -> productPages.add(getPageByUrl(a.attr("href"))));
+        }
+    }
+
+    /**
+     * The method parse given product page, extract data from it into ProductDto object
+     * and put the object into the List.
+     *
+     * @param el product page
+     */
+    private void extractProductAndAddToList(Document el) {
+        String fullProductName = el.select("div.left > h1 > span").first().text();
+        String description = getDescription(el);
+        //skip  products which marked as "Open box" or as "витринный вариант"
+        if (fullProductName.toLowerCase().contains("open box")
+                || description.toLowerCase().contains("витринный вариант")) {
+            return;
+        }
+
+        //download product image by url, save to project folder and provide path to it
+        Optional<String> pathToSavedImageOptional = saveImage(getImageUrl(el));
+        String pathToImage = null;
+        if (pathToSavedImageOptional.isPresent()) {
+            pathToImage = pathToSavedImageOptional.get();
+        }
+        String model = getModelFromFullProductName(fullProductName);
+        products.add(ProductDtoBuilder.aProductDto()
+                .withName(getShortProductName(fullProductName, model))
+                .withModel(model)
+                .withImage(pathToImage)
+                .withDescription(description)
+                .withYear(getYear(fullProductName))
+                .withProductProperties(getProperties(el))
+                .build());
+    }
+
+    /**
+     * Get model from full product name.
+     * For example, model from "Смартфон Apple iPhone 11 Pro 64GB Space Gray (MWC22)" will look like
+     * "MWC22".
+     *
+     * @param fullProductName a full name of a product
+     * @return string with model
+     */
+    private String getModelFromFullProductName(String fullProductName) {
+        Matcher matcher = Pattern.compile("\\([MZ].*\\)").matcher(fullProductName);
+        String model = null;
+        while (matcher.find()) {
+            model = fullProductName.substring(matcher.start(), matcher.end()).replaceAll("[()]", "");
+        }
+        return model;
+    }
+
+    /**
+     * Getting normal product name by cutting cyrillic prefix and model number.
+     * For example, "Смартфон Apple iPhone 11 Pro 64GB Space Gray (MWC22)" will be cut to
+     * "Apple iPhone 11 Pro 64GB Space Gray".
+     *
+     * @param fullProductName a full name of a product
+     * @return short name of the product.
+     */
+    private String getShortProductName(String fullProductName, String model) {
+        String nameWithoutCyrillic = fullProductName.substring(fullProductName.indexOf(' ') + 1);
+        String nameWithoutModel = nameWithoutCyrillic;
+        if (model != null) {
+            nameWithoutModel = nameWithoutCyrillic.replace(model, "");
+        }
+
+        return nameWithoutModel
+                .replaceAll("[()]", "")
+                .trim()
+                .replaceAll(" +", " ");
+    }
+
+    /**
+     * Get src link of the image at the product page, and add Avic domain name to the get absolute path.
+     *
+     * @param el a product page
+     * @return the absolute image url.
+     */
+    private String getImageUrl(Element el) {
+        return AVIC_DOMAIN.concat(el.select("span > a > img.js_main-img").first().attr("src"));
+    }
+
+    private Optional<String> saveImage(String url) {
+        return new ImageSaverImpl().saveImageByUrlToDefaultFolder(url);
+    }
+
+    /**
+     * On Avic site the description of a product is split into a few paragraphs.
+     * We iterate through them and unit into the normal description.
+     *
+     * @param el a product page
+     * @return description of the product
+     */
+    private String getDescription(Element el) {
+        Elements paragraphsWithDescription = el.select("div.about-product > p");
+        StringBuilder builder = new StringBuilder();
+        for (Element paragraph : paragraphsWithDescription) {
+            builder.append(" ");
+            builder.append(paragraph.text());
+        }
+        return builder.toString().trim().replaceAll(" +", " ");
+    }
+
+    /**
+     * Gets a year of product made from full product name using regex.
+     *
+     * @param fullProductName a full product name
+     * @return a year of product made or null if the year is absent in product name
+     */
+    private Year getYear(String fullProductName) {
+        Matcher matcher = Pattern.compile("(?:\\(|\\s)20\\d{2}").matcher(fullProductName);
+        String year = null;
+        while (matcher.find()) {
+            year = fullProductName.substring(matcher.start(), matcher.end());
+        }
+        return year == null ? null : Year.of(Integer.parseInt(year.substring(1)));
+    }
+
+    private ProductPropertiesDto getProperties(Element el) {
+        Elements rowsWithProperties = el.select("div.table-features > table > tbody > tr");
+        return ProductPropertiesDtoBuilder.aProductPropertiesDto()
+                .withProperties(getPropertiesAsJsonString(rowsWithProperties))
+                .build();
+    }
+
+    private String getPropertiesAsJsonString(Elements elements) {
+        StringBuilder builder = new StringBuilder("{");
+        for (Element r : elements) {
+            Elements keyAndValueEl = r.select("td");
+            String key = keyAndValueEl.get(0).text();
+            String value = keyAndValueEl.get(1).text();
+
+            //skip rows with headers
+            if (key.isEmpty() || value.isEmpty()) {
+                continue;
+            }
+
+            builder.append("\"");
+            builder.append(key.replace(":", ""));
+            builder.append("\" : \"");
+            builder.append(value.replace("\"", "'"));
+            builder.append("\",");
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append("}");
+        return builder.toString();
+    }
+}
