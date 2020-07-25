ce073e78cd8a643ed11929d2299e5efa65600f2d
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2020-07-26 01:52:16 +0300

    PO-80 - refactoring.

--- webapp/src/main/java/priceobserver/util/LayoutUtils.java
+++ webapp/src/main/java/priceobserver/util/LayoutUtils.java
@@ -6,7 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class LayoutUtils {
+public final class LayoutUtils {
+
+    public static final String SELECTED_PAGE_ATTR = "selectedPage";
+    public static final String PAGE_LIST_ATTR = "pageList";
+    public static final String PREVIOUS_PAGE_ATTR = "previousPage";
+    public static final String NEXT_PAGE_ATTR = "nextPage";
+
+    public static final String THREE_DOTS = "...";
+    public static final String FIRST = "1";
+    public static final String SECOND = "2";
+    public static final String THIRD = "3";
+    public static final String FOURTH = "4";
+    public static final String FIFTH = "5";
 
     public static final String PRODUCT_PAGE = "product";
     public static final String PRODUCT_LIST_PAGE = "productsList";
@@ -17,14 +29,14 @@ public class LayoutUtils {
     public static final byte NUMBER_OF_PRODUCTS_PER_PAGE_AT_A_TIME = 9;
 
     private LayoutUtils() {
-        //prevent object of utils classes
+
     }
 
     public static void preparePagination(Model model, Integer selectedPage, int countOfPages) {
-        model.addAttribute("selectedPage", selectedPage.toString());
-        model.addAttribute("pageList", LayoutUtils.getPaginationList(selectedPage, countOfPages));
-        model.addAttribute("previousPage", selectedPage - 1 < 1 ? "" : String.valueOf(selectedPage - 1));
-        model.addAttribute("nextPage", selectedPage + 1 > countOfPages ? "" : String.valueOf(selectedPage + 1));
+        model.addAttribute(SELECTED_PAGE_ATTR, selectedPage.toString());
+        model.addAttribute(PAGE_LIST_ATTR, LayoutUtils.getPaginationList(selectedPage, countOfPages));
+        model.addAttribute(PREVIOUS_PAGE_ATTR, selectedPage - 1 < 1 ? "" : String.valueOf(selectedPage - 1));
+        model.addAttribute(NEXT_PAGE_ATTR, selectedPage + 1 > countOfPages ? "" : String.valueOf(selectedPage + 1));
     }
 
     private static List<String> getPaginationList(int selectedPage, int countOfPages) {
@@ -38,36 +50,35 @@ public class LayoutUtils {
             return pageList;
         }
 
-        pageList.add("1");
-
+        pageList.add(FIRST);
         if (selectedPage == 1) {
             for (int i = 2; i < 6; i++) {
                 pageList.add(String.valueOf(i));
             }
-            pageList.add("...");
+            pageList.add(THREE_DOTS);
         } else if(selectedPage == countOfPages) {
             for (int i = countOfPages - 4; i < countOfPages; i++) {
                 pageList.add(String.valueOf(i));
             }
-            pageList.add(1, "...");
+            pageList.add(1, THREE_DOTS);
         } else if (selectedPage <= 4) {
             pageList.addAll(
-                    List.of("2","3", "4", "5", "...")
+                    List.of(SECOND, THIRD, FOURTH, FIFTH, THREE_DOTS)
             );
         } else if (selectedPage >= countOfPages - 3) {
             pageList.addAll(
-                    List.of("...",
+                    List.of(THREE_DOTS,
                             String.valueOf(countOfPages - 4),
                             String.valueOf(countOfPages - 3),
                             String.valueOf(countOfPages - 2),
                             String.valueOf(countOfPages - 1))
             );
         } else {
-            pageList.addAll(List.of("...",
+            pageList.addAll(List.of(THREE_DOTS,
                     String.valueOf(selectedPage - 1),
                     String.valueOf(selectedPage),
                     String.valueOf(selectedPage + 1),
-                    "...")
+                    THREE_DOTS)
             );
 
         }
